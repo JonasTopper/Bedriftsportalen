427cdd8ff82e1a2d12e33e409dc95697c59db0e0
JonasTopper <Jonas.topper@gmail.com>
2024-03-05 10:12:09 +0100

    Push first documents to main

---
+++ Update.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/stylesheet.css">
+    <title> Bedriftsportalen </title>
+</head>
+<body>
+    
+
+    <h1> Bedriftsportalen </h1>
+
+    <a href="Create.php"><button type="button" class="btn"> Create</button></a>
+    <a href="Read.php"><button type="button" class="btn"> Read</button></a>
+    <a href="Update.php"><button type="button" class="btn"> Update</button></a>
+    <a href="Delete.php"><button type="button" class="btn"> Delete</button></a>
+
+
+    <script src="JavaScript/script.js">
+</body>
+</html>
