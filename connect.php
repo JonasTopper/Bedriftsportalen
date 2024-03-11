f221bc6b40a14a8fa4d6739f324e2639a83dc734
Ariel <arielfernandezheggernes@gmail.com>
2024-03-11 10:02:15 +0100

    Connect file added

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+
+// Parametere til databasen
+$host = 'localhost';
+$brukernavn = 'root';
+$passord = '';
+$database = 'bedriftsportalen_db';
+
+// Lag en forindelses-streng
+$conn = mysqli_connect($host, $brukernavn, $passord, $database);
+
+// Sjekk Forbindelse
+
+if(!$conn)
+  {
+    die('Feil med forbindelsen'. mysqli_connect_error());
+  }
+else
+  {
+   //echo "Databasen er tilkoblet";
+  }
