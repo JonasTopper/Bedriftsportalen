1f6771346377ca7a184a4f401f740cee484e6db0
JonasTopper <Jonas.topper@gmail.com>
2024-03-12 08:37:51 +0100

    Added buttons for modifying 'bedrifter'

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 include 'CRUD/connect.php';
+
+
+
+// Prosedyre for les
+$sql_les = "SELECT bedrift_id, navn FROM bedrifter";
+$resultat_les = mysqli_query($conn, $sql_les);
+$bedrifter = mysqli_fetch_all($resultat_les, MYSQLI_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -8,18 +16,34 @@ include 'CRUD/connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-pwCHXNHXDBp4Zh3fCkGpMeWzHjwUC1n1br5x3IyVBFzbJRsN/l2M+SWdgZfjqxiS" crossorigin="anonymous">
     <title>Bedriftsportalen</title>
 </head>
 <body>
     <h1>Bedriftsportalen</h1>
 
-    <main>
-        <?php
-        // Prosedyre for les
-        $sql_les = "SELECT bedrift_id, navn FROM bedrifter";
-        $resultat_les = mysqli_query($conn, $sql_les);
-        $bedrifter = mysqli_fetch_all($resultat_les, MYSQLI_ASSOC);
+        <div class="create-delete-edit">
 
+            <div class="create-btn">
+                <a href="x.php" ><button> + </button></a>
+            </div>
+
+            <div class="delete-btn">
+                <a href="x.php" ><button> Remove </button></a>
+            </div>
+
+            <div class="edit-btn">
+                <a href="x.php" ><button> Edit </button></a>
+            </div>
+
+        </div>
+    <main>
+
+
+
+
+
+        <?php
         foreach($bedrifter as $bedrift) {
             $navn = $bedrift['navn'];
             $bedrift_id = $bedrift['bedrift_id'];
