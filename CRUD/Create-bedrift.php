f3159b78eb0d775ad10d882b9b7813fc7bd7a874
Oliver DM <60272851+MajorGeneralCommand@users.noreply.github.com>
2024-04-02 08:45:39 +0200

    Update Create-bedrift.php
    Nå sendes over bedrift poststed og!

--- CRUD/Create-bedrift.php
+++ CRUD/Create-bedrift.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])) {
     $result = mysqli_query($conn, $sql);
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $poststed = $row['bedrift_post_sted'];
+        $poststed = $row['poststed'];
     } else {
         $poststed = ""; // Set default value or handle error
     }
