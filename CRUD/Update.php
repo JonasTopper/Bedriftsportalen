e00516c6613af539973a37f0e907c673bdba7c46
Ariel <arielfernandezheggernes@gmail.com>
2024-04-02 09:03:18 +0200

    Font styling to all websites
    Including script fix

--- CRUD/Update.php
+++ CRUD/Update.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../stylesheets/stylesheet.css?v=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-pwCHXNHXDBp4Zh3fCkGpMeWzHjwUC1n1br5x3IyVBFzbJRsN/l2M+SWdgZfjqxiS" crossorigin="anonymous">
     <title> Bedriftsportalen </title>
 </head>
 <body>
@@ -18,6 +19,6 @@
     <a href="../"><button type="button" class="btn"> Index</button></a>
     </main>
 
-    <script src="JavaScript/script.js">
+    <script src="JavaScript/script.js"></script>
 </body>
 </html>
