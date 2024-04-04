e768a7aa0f8eee57143ef147ccdeda921c522458
JonasTopper <Jonas.topper@gmail.com>
2024-04-04 13:31:04 +0200

    Added popup for tos
    Cool shit

---
+++ terms_of_service.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms of Service Bedriftsportalen</title>
+    <link rel="stylesheet" href="stylesheets/stylesheet.css">
+</head>
+<body>
+    <main class="terms_of_service">
+        <p>
+
+
+
+<b>MIT License</b><br><br>
+<br>
+Copyright (c) 2024 Arielmfh & JonasTopper<br><br>
+<br>
+Permission is hereby granted, free of charge, to any person obtaining a copy<br>
+of this software and associated documentation files (the "Software"), to deal<br>
+in the Software without restriction, including without limitation the rights<br>
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br>
+copies of the Software, and to permit persons to whom the Software is<br>
+furnished to do so, subject to the following conditions:<br>
+<br>
+The above copyright notice and this permission notice shall be included in all<br>
+copies or substantial portions of the Software.<br>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br>
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br>
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br>
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br>
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br>
+SOFTWARE.<br>
+<br>
+<br>
+--------------------------------------------------------------------------------<br>
+<br>
+<br>
+Terms of use for Bedriftsportalen<br>
+<br>
+Date of last update: 20/03/2024<br>
+<br>
+These terms of use govern your access to and use of the Company Portal offered by us. Please read these terms carefully before using the Service.<br>
+<br>
+1. Acceptance of Terms<br>
+<br>
+By registering for or using the Service, you agree to be bound by these Terms. If you do not agree to any of the Terms, you must not use or access the Service.<br>
+<br>
+2. Description of the service<br>
+<br>
+The service is a Customer Relationship Management (CRM) system offered as a tool for companies to manage their customer information, contact information and other relevant data related to their business activities.<br>
+<br>
+3. User information<br>
+<br>
+By using the Service, you agree to provide accurate and up-to-date information about yourself and your company, including, but not limited to, name, email address, address, zip code, post office, position in company and name of company.<br>
+<br>
+4. Use of Data and GDPR compliance<br>
+<br>
+We undertake to process the Data you provide in accordance with applicable privacy laws, including the General Data Protection Regulation (GDPR). The data will be used exclusively for the purposes of providing, improving and maintaining the Service. We will not share, sell or transfer Data without your consent, unless required by law.<br>
+<br>
+5. Confidentiality<br>
+<br>
+We undertake to maintain the confidentiality of all information you provide through the Service. We will not disclose or share such information with third parties without your consent, unless required by law.<br>
+<br>
+6. Disclaimer<br>
+<br><br>
+We make no guarantees as to the accuracy, reliability or completeness of the Data provided through the Service. You understand and accept that you use the Service at your own risk, and that We shall not be held responsible for any losses or damages arising as a result of your use of the Service.<br>
+<br>
+7. Changes to the Terms<br>
+<br>
+We reserve the right to change these terms at any time. Any changes will be notified by publishing the revised terms on our website. By continuing to use the Service after the changes take effect, you agree to be bound by the revised terms.<br>
+<br>
+8. Contact us
+<br>
+If you have any questions or concerns regarding these Terms, please contact us.
+<br><br><br><br><br><br>
+</p>
+ <main>
+<a href="index.php"> <button class="tilbake_fra_terms" type="button"> Tilbake </button></a>
+
+
+   
+</body>
+</html>
