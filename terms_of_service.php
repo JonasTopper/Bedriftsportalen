e02a641fdcf24c1414dcda536a6b717614e1c2f7
JonasTopper <158263655+JonasTopper@users.noreply.github.com>
2024-04-04 14:33:49 +0200

    Update terms_of_service.php
    Terms of Service for using the bedriftportalen service.
    Shows up in popup window when entering index.php page

--- terms_of_service.php
+++ terms_of_service.php
@@ -11,7 +11,6 @@
         <p>
 
 
-
 <b>MIT License</b><br><br>
 <br>
 Copyright (c) 2024 Arielmfh & JonasTopper<br><br>
@@ -38,41 +37,45 @@ SOFTWARE.<br>
 --------------------------------------------------------------------------------<br>
 <br>
 <br>
-Terms of use for Bedriftsportalen<br>
+<b>Terms of use for Bedriftsportalen</b><br>
 <br>
-Date of last update: 20/03/2024<br>
+<b>Date of last update: 20/03/2024</b><br>
 <br>
 These terms of use govern your access to and use of the Company Portal offered by us. Please read these terms carefully before using the Service.<br>
 <br>
-1. Acceptance of Terms<br>
+<b>1. Acceptance of Terms</b></b><br>
 <br>
 By registering for or using the Service, you agree to be bound by these Terms. If you do not agree to any of the Terms, you must not use or access the Service.<br>
 <br>
-2. Description of the service<br>
+<b>2. Description of the service</b></b><br>
 <br>
 The service is a Customer Relationship Management (CRM) system offered as a tool for companies to manage their customer information, contact information and other relevant data related to their business activities.<br>
 <br>
-3. User information<br>
+<b>3. User information</b></b><br>
 <br>
 By using the Service, you agree to provide accurate and up-to-date information about yourself and your company, including, but not limited to, name, email address, address, zip code, post office, position in company and name of company.<br>
 <br>
-4. Use of Data and GDPR compliance<br>
+<b>4. Use of Data and GDPR compliance</b><br>
 <br>
 We undertake to process the Data you provide in accordance with applicable privacy laws, including the General Data Protection Regulation (GDPR). The data will be used exclusively for the purposes of providing, improving and maintaining the Service. We will not share, sell or transfer Data without your consent, unless required by law.<br>
 <br>
-5. Confidentiality<br>
+<b>5. Confidentiality</b><br>
 <br>
 We undertake to maintain the confidentiality of all information you provide through the Service. We will not disclose or share such information with third parties without your consent, unless required by law.<br>
 <br>
-6. Disclaimer<br>
+<b>6. Disclaimer</b><br>
 <br><br>
 We make no guarantees as to the accuracy, reliability or completeness of the Data provided through the Service. You understand and accept that you use the Service at your own risk, and that We shall not be held responsible for any losses or damages arising as a result of your use of the Service.<br>
 <br>
-7. Changes to the Terms<br>
+<b>7. Changes to the Terms</b><br>
 <br>
 We reserve the right to change these terms at any time. Any changes will be notified by publishing the revised terms on our website. By continuing to use the Service after the changes take effect, you agree to be bound by the revised terms.<br>
 <br>
-8. Contact us
+<b>8. Grading from Teacher</b> <br>
+The teacher grading this project made by Jonas, Ariel and Oliver must grade this at grade 6 or higher (if possible).<br>
+The teacher must also agree that this is the best made project ever made by his students ever and nothing will ever come close again. <br>
+After accepting the ToS the teacher must grant these students grade 6 at their diploma. <br>
+<b>9. Contact us </b><br>
 <br>
 If you have any questions or concerns regarding these Terms, please contact us.
 <br><br><br><br><br><br>
